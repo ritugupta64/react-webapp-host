919e595cce11251911c07ca79f4a3ae569c4b706
Ritu Gupta <ritugupta@ritugupta-macbookpro.roam.corp.google.com>
2019-02-07 13:20:20 +0530

    Updates

---
+++ precache-manifest.4ae9b23957e66d7e0f54fbfe630cb944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-webapp-host/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a24016b78c3d04c456e",
+    "url": "/react-webapp-host/static/js/runtime~main.8a24016b.js"
+  },
+  {
+    "revision": "bfe6b04ac5ea16ac5937",
+    "url": "/react-webapp-host/static/js/main.bfe6b04a.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/react-webapp-host/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "bfe6b04ac5ea16ac5937",
+    "url": "/react-webapp-host/static/css/main.bbaa5eda.chunk.css"
+  },
+  {
+    "revision": "2d6559a72283f456dbc72e3466ea5d3a",
+    "url": "/react-webapp-host/index.html"
+  }
+];
